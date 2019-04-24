b3d23ca7c5ed740b2fe44edaa532a64e5a68dc27
rachelroll <hai30@163.com>
2019-04-24 14:49:58 +0800

    Add timeline

--- pages/offline/index.js
+++ pages/offline/index.js
@@ -5,15 +5,107 @@ const wxGetImageInfo = promisify(wx.getImageInfo)
 
 
 
+
 Page({
 
     /**
      * Page initial data
      */
-data: {
-    path:'../../images/123.jpg'
-    },
+    data: {
+        TabCur: 0,
+        scrollLeft:0,
+        months:[
+            '一月',
+            '二月',
+            '三月',
+            '四月',
+            '五月',
+            '六月',
+            '七月',
+            '八月',
+            '九月',
+            '十月',
+            '十一月',
+            '十二月',
+        ],
 
+        // 数据结构
+        timeline_data:{
+            0:{   //普通的键  index ,对应上面月份的键 
+                '05':[   // day 比如一月份 05号
+                    {
+                        'meridiem':'上午',
+                        'time':'10:00',
+                        'title':'2年,我把公司搞上了市',
+                        'position':'中国 北京 西单商场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    },
+                    {
+                        'meridiem':'下午',
+                        'time':'15:00',
+                        'title':'2年,我把公司搞上了市',
+                        'position':'中国 北京 西单商场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    }
+                ],
+                '23':[  // day 比如一月份 23号
+                    {
+                        'meridiem':'上午',
+                        'time':'10:00',
+                        'title':'3年,我去了京东商城',
+                        'position':'中国 北京 西单商场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    }
+                ],
+                '30':[
+                    {
+                        'meridiem':'上午',
+                        'time':'10:00',
+                        'title':'3年,我去了京东商城',
+                        'position':'中国 北京 西单商场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    }
+                ]
+            },
+            1:{
+                '12':[
+                    {
+                        'meridiem':'上午',
+                        'time':'10:00',
+                        'title':'2年,我把公司搞上了市',
+                        'position':'中国 北京 西单商场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    },
+                    {
+                        'meridiem':'下午',
+                        'time':'15:00',
+                        'title':'2年,我把公司搞上了市',
+                        'position':'中国 北京 西单商场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    }
+                ],
+
+            },
+            2:{
+                '27':[
+                    {
+                        'meridiem':'上午',
+                        'time':'10:00',
+                        'title':'京北读书会',
+                        'position':'中国 北京 天安门北广场',
+                        'desc':'该活动纯属吹牛,请不要相信.哈哈哈哈哈',
+                    }
+                ],
+
+            }
+        }
+    },
+    tabSelect(e) {
+        this.setData({
+            TabCur: e.currentTarget.dataset.id,
+            scrollLeft: (e.currentTarget.dataset.id-1)*60
+        })
+    },
     /**
      * Lifecycle function--Called when page load
      */
